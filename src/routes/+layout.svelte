3dc3cebad6db6e0a4dc0b5442379bc5a14d8febe
Lyssa Li <lyssa.li137@gmail.com>
2025-04-01 17:10:55 -0700

    fixing base page layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,60 +1,57 @@
 <script lang="ts">
-  import { base } from "$app/paths";
-  import { page } from "$app/state";
-  import "@fontsource/anton";
-  import "@fontsource/lato";
+	import { base } from "$app/paths";
+	import { page } from "$app/state";
+	import "@fontsource/anton";
+	import "@fontsource/lato";
 
-  let { children } = $props();
+	let { children } = $props();
 
-  const pages: string[] = ["art"];
-  console.log(page.url.pathname, base);
+	const pages: string[] = ["art", "code"];
 </script>
 
 {#key page.url.pathname}
-  {#if [`${base}`, "/"].includes(page.url.pathname)}
-		 <div class="col everything-bagel">
+	{#if page.url.pathname === `${base}/`}
+		<div class="col everything-bagel">
+			<h1>style guides for galactic coffee shop</h1>
+			<p>mostly a hub to collect style guides in one place</p>
+			<br />
 
-    <h1>style guides for galactic coffee shop</h1>
-    <p>mostly a hub to collect style guides in one place</p>
-    <br />
-
-    <div class="row">
-      {#each pages as p (p)}
-        <a href="{base}/{p}">
-          <button>{p}</button>
-        </a>
-      {/each}
-    </div>
-
-		 </div>
-  {:else}
-    {@render children()}
-  {/if}
+			<div class="row">
+				{#each pages as p (p)}
+					<a href="{base}/{p}">
+						<button>{p}</button>
+					</a>
+				{/each}
+			</div>
+		</div>
+	{:else}
+		{@render children()}
+	{/if}
 {/key}
 
 <style>
-	 .everything-bagel {
+	.everything-bagel {
 		height: 100%;
 		width: 100%;
 		justify-content: center;
 		align-items: center;
 		overflow: hidden;
 	}
-  h1 {
-    font-family: "Anton", sans-serif;
-  }
-  * {
-    font-family: "Lato", sans-serif;
-  }
-  button {
-    cursor: pointer;
-    padding: 0.5rem;
-    background-color: transparent;
-    color: var(--text);
-    border: 1px solid var(--text);
-    &:hover {
-      border-color: var(--ac);
-      color: var(--ac);
-    }
-  }
+	h1 {
+		font-family: "Anton", sans-serif;
+	}
+	* {
+		font-family: "Lato", sans-serif;
+	}
+	button {
+		cursor: pointer;
+		padding: 0.5rem;
+		background-color: transparent;
+		color: var(--text);
+		border: 1px solid var(--text);
+		&:hover {
+			border-color: var(--ac);
+			color: var(--ac);
+		}
+	}
 </style>
